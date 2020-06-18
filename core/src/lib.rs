8fd394c144874baa8f0182da2f3b5d1dd0864603
Matej <dobrakmato@gmail.com>
2020-06-18 18:55:03 +0200

    core: add `assert_alignment` macro

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -2,3 +2,32 @@
 
 pub mod notification;
 pub mod perf;
+
+/// Statically asserts that the alignment of specified type is
+/// specified number of bytes.
+///
+/// # Example
+///
+/// This code will compile just fine.
+///
+/// ```
+/// # use core::assert_alignment;
+/// assert_alignment!(u8, 1);
+/// assert_alignment!(u16, 2);
+/// assert_alignment!(u32, 4);
+/// ```
+///
+/// This will however fail to compile as alignemnt of `u32` is not 8 bytes.
+///
+/// ```compile_fail
+/// # use core::assert_alignment;
+/// assert_alignment!(u32, 8);
+/// ```
+#[macro_export]
+macro_rules! assert_alignment {
+    ($typ:ty, $to:expr) => {
+        const _: fn() = || {
+            let _: [(); std::mem::align_of::<$typ>()] = [(); $to];
+        };
+    };
+}
