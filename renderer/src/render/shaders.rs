7b00057b12d68a5827890065e6bcedb195a2235e
Matej <dobrakmato@gmail.com>
2021-04-20 00:00:45 +0200

    renderer: remove hack used to force cargo to recompile the shaders

--- renderer/src/render/shaders.rs
+++ renderer/src/render/shaders.rs
@@ -1,6 +1,4 @@
 pub mod vs_deferred_geometry {
-    #[allow(dead_code)] // Used to force recompilation of shader change
-    const X: &str = include_str!("../../shaders/vs_deferred_geometry.glsl");
     vulkano_shaders::shader! {
         ty: "vertex",
         path: "shaders/vs_deferred_geometry.glsl"
@@ -8,8 +6,6 @@ pub mod vs_deferred_geometry {
 }
 
 pub mod fs_deferred_geometry {
-    #[allow(dead_code)] // Used to force recompilation of shader change
-    const X: &str = include_str!("../../shaders/fs_deferred_geometry.glsl");
     vulkano_shaders::shader! {
         ty: "fragment",
         path: "shaders/fs_deferred_geometry.glsl"
@@ -17,8 +13,6 @@ pub mod fs_deferred_geometry {
 }
 
 pub mod fs_deferred_lighting {
-    #[allow(dead_code)] // Used to force recompilation of shader change
-    const X: &str = include_str!("../../shaders/fs_deferred_lighting.glsl");
     vulkano_shaders::shader! {
         ty: "fragment",
         path: "shaders/fs_deferred_lighting.glsl"
@@ -26,8 +20,6 @@ pub mod fs_deferred_lighting {
 }
 
 pub mod vs_passtrough {
-    #[allow(dead_code)] // Used to force recompilation of shader change
-    const X: &str = include_str!("../../shaders/vs_passtrough.glsl");
     vulkano_shaders::shader! {
         ty: "vertex",
         path: "shaders/vs_passtrough.glsl"
@@ -35,8 +27,6 @@ pub mod vs_passtrough {
 }
 
 pub mod fs_tonemap {
-    #[allow(dead_code)] // Used to force recompilation of shader change
-    const X: &str = include_str!("../../shaders/fs_tonemap.glsl");
     vulkano_shaders::shader! {
         ty: "fragment",
         path: "shaders/fs_tonemap.glsl"
